385c59540d35cce2a922b24a39ef80071be9af35
Atul Mane <atul@boldcare.in>
2023-03-13 18:05:02 +0530

    complete: concurrency

---
+++ 16. Fearless Concurrency/using_message_passing/src/main.rs
@@ -0,0 +1,38 @@
+use std::time::Duration;
+use std::{sync::mpsc, thread};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
